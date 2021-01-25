8676efff88411a7f0e3ee6a3ea9e71ccc9869538
chloe <1605hjk@gmail.com>
2021-01-25 16:52:34 +0900

    'React'

--- src/AllExample02.js
+++ src/AllExample02.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Welcome from './Example02/01props+state';
+import Welcome from './Example02/01props';
+import App from './Example02/02state';
 
 export default function AllExample02() {
     return(
         <>
-        <Welcome name='Maso' age='10'/>
+        {/* <Welcome name='Maso' age='10'/> */}
+        <App />
         </>
-    )
-}
+    );
+};
--- src/Example02/01props.js
+++ src/Example02/01props.js
@@ -1,7 +1,7 @@
 export default function Welcome(props) {
     return (
         <>
-        <h1>hello, {props.name}</h1>
+        <h1>Hello I am {props.name}</h1>
         <h1>My age is {props.age}</h1>
         </>
     );
