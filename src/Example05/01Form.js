a06feb4a730f34534b2dcdf88526dfa1da31676d
chloe <1605hjk@gmail.com>
2021-01-29 14:22:49 +0900

    React Form Example

--- src/Example05/01Form.js
+++ src/Example05/01Form.js
@@ -1,8 +1,33 @@
+import React, { useState } from 'react';
+ 
 export default function Form() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // 1. form에서 submit 할 때마다 리프레시 되는 것
+    const onSubmit1 = () => {
+        alert('로그인 됐습니다.');
+    }
+
+    // 2. form에서 엔터 칠 때, 리프레시 되지 않도록
+    const onSubmit2 = (event) => {
+        event.preventDefault();
+        console.log(username,password);
+    };
+
     return (
-        <form>
-            <input name='password' />
-            <button type='submit'>비밀번호 변경</button>
-        </form>
-    )
+        <div className='container'>
+            <form onSubmit={onSubmit1}>
+                <input 
+                    placeholder="Username" 
+                    value={username} 
+                    onChange={(e)=>setUsername(e.target.value)}/><br/>
+                <input 
+                    placeholder="Password" 
+                    value={password} 
+                    onChange={(e)=>setPassword(e.target.value)}/><br/>
+                <button type='submit'>로그인</button>
+            </form>
+        </div>  
+        )
 };
